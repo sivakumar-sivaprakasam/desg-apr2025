93a3db84755e188f120ddd5a60b85f1fd2b3daf1
madlyrust <s.sivakumar77@hotmail.com>
2025-04-20 19:18:01 +0800

    Removed commented code

--- polars_df_eager.py
+++ polars_df_eager.py
@@ -13,23 +13,14 @@ pl.Config.set_thousands_separator(",")
 # Load dataset into a DataFrame
 df = pl.read_csv("SG_HDB_Resale_Flat_Prices.csv", separator=",", infer_schema_length=1_000_000)
 
-# Get the facts 
-#print(df.group_by(pl.col("flat_type")).len().sort(pl.col("flat_type")))
-
 # Filter un-necessary data
 excluded_flat_types = ["null", "1 ROOM", "2 ROOM"]
 df = df.filter(~pl.col("flat_type").is_in(excluded_flat_types))
-
-# Verify the data set post filter operation
-#print(df.group_by(pl.col("flat_type")).len().sort(pl.col("flat_type")))
 
 # Data Standardization
 df = df.with_columns(flat_type = pl.when(pl.col("flat_type") == "MULTI-GENERATION")
                      .then(pl.lit("MULTI GENERATION"))
                      .otherwise(pl.col("flat_type")))
-
-# Verify the data set post data standardization operation
-#print(df.group_by(pl.col("flat_type")).len().sort(pl.col("flat_type")))
 
 # For each flat type, find Minimum & Maximum resale valued transaction from data scope
 df_agg_flat_types = df.group_by(pl.col("flat_type")).agg(
@@ -37,13 +28,6 @@ df_agg_flat_types = df.group_by(pl.col("flat_type")).agg(
         pl.col("resale_price").max().alias("max_resale_value")
         ).sort(pl.col("flat_type"))
 print(df_agg_flat_types)
-
-# For each town & flat type, find Minimum & Maximum resale valued transaction from data scope
-#df_agg_town_flat_types = df.group_by(pl.col("town"), pl.col("flat_type")).agg(
-#        pl.col("resale_price").min().alias("min_resale_value"),
-#        pl.col("resale_price").max().alias("max_resale_value")
-#        ).sort(pl.col("town"), pl.col("flat_type"))
-#print(df_agg_town_flat_types)
 
 # For each town & flat type, find Minimum & Maximum resale valued transaction from data scope
 df_min = df.pivot("flat_type", index="town", values="resale_price", aggregate_function="min")
