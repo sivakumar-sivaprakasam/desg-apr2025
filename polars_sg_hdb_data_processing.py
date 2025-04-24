01fbb3fdf0b96aa527868259db771c42cb1f5d8f
madlyrust <s.sivakumar77@hotmail.com>
2025-04-24 22:20:23 +0800

    Renamed script file name

--- polars_sg_hdb_data_processing.py
+++ polars_sg_hdb_data_processing.py
@@ -11,42 +11,67 @@ pl.Config.set_fmt_float("full")
 pl.Config.set_thousands_separator(",")
 
 # Load dataset into a DataFrame
-df = pl.read_csv("data/SG_HDB_Resale_Flat_Prices.csv", separator=",", infer_schema_length=1_000_000)
+df = pl.read_csv("data\SG_HDB_Resale_Flat_Prices.csv", separator=",", infer_schema_length=1_000_000)
 
 # Filter un-necessary data
 excluded_flat_types = ["null", "1 ROOM", "2 ROOM"]
 df = df.filter(~pl.col("flat_type").is_in(excluded_flat_types))
 
 # Data Standardization
-df = df.with_columns(flat_type = pl.when(pl.col("flat_type") == "MULTI-GENERATION").then(pl.lit("MULTI GENERATION"))
+df = df.with_columns(flat_type = pl                     
                      .when(pl.col("flat_type") == "3 ROOM").then(pl.lit("3RM"))
                      .when(pl.col("flat_type") == "4 ROOM").then(pl.lit("4RM"))
                      .when(pl.col("flat_type") == "5 ROOM").then(pl.lit("5RM"))
                      .when(pl.col("flat_type") == "EXECUTIVE").then(pl.lit("EXEC"))
+                     .when(pl.col("flat_type") == "MULTI-GENERATION").then(pl.lit("MULTI-GEN"))
                      .when(pl.col("flat_type") == "MULTI GENERATION").then(pl.lit("MULTI-GEN")),
-                     decade = pl.col("month").str.to_date("%Y-%m").dt.year() // 10 * 10)
 
-print("Min & Max Resale Values for each flat type for the period 1990 - Till date")
+                     decade = pl.col("month").str.to_date("%Y-%m").dt.year() // 10 * 10
+)
+
 # For each flat type, find Minimum & Maximum resale valued transaction from data scope
+print("Min & Max Resale Values for each flat type for the period 1990 - Till date")
+
 df_agg_flat_types = df.group_by(pl.col("flat_type")).agg(
         pl.col("resale_price").min().alias("min_resale_value"),
         pl.col("resale_price").max().alias("max_resale_value")
-        ).sort(pl.col("flat_type"))
+).sort("flat_type")
+
 print(df_agg_flat_types)
 
-print("Min & Max Resale Values for each flat type from each town & decade for the period 1990 - Till date")
 # For each town & flat type, find Minimum & Maximum resale valued transaction from data scope
+print("Min & Max Resale Values for each flat type from each town & decade for the period 1990 - Till date")
+
 df_min = df.pivot("flat_type", index=["town", "decade"], values="resale_price", aggregate_function="min")
 df_max = df.pivot("flat_type", index=["town", "decade"], values="resale_price", aggregate_function="max")
+
 df_joined = df_min.join(df_max, on=["town", "decade"]).select(
     pl.col("town"), 
     pl.format("{}", pl.col("decade")),
-    pl.col("3RM").alias("3RM (MIN)"), pl.col("3RM_right").alias("3RM (MAX)"), 
-    pl.col("4RM").alias("4RM (MIN)"), pl.col("4RM_right").alias("4RM (MAX)"), 
-    pl.col("5RM").alias("5RM (MIN)"), pl.col("5RM_right").alias("5RM (MAX)"), 
-    pl.col("EXEC").alias("EXEC (MIN)"), pl.col("EXEC_right").alias("EXEC (MAX)"), 
-    pl.col("MULTI-GEN").alias("MULTI-GEN (MIN)"), pl.col("MULTI-GEN_right").alias("MULTI-GEN (MAX)")).sort("town", "decade")
+
+    # 3RM (MIN) & 3RM (MAX)
+    pl.col("3RM").alias("3RM (MIN)"), 
+    pl.col("3RM_right").alias("3RM (MAX)"), 
+
+    # 4RM (MIN) & 4RM (MAX)
+    pl.col("4RM").alias("4RM (MIN)"), 
+    pl.col("4RM_right").alias("4RM (MAX)"), 
+
+    # 5RM (MIN) & 5RM (MAX)
+    pl.col("5RM").alias("5RM (MIN)"), 
+    pl.col("5RM_right").alias("5RM (MAX)"), 
+
+    # EXEC (MIN) & EXEC (MAX)
+    pl.col("EXEC").alias("EXEC (MIN)"), 
+    pl.col("EXEC_right").alias("EXEC (MAX)"), 
+
+    # MULTI-GEN (MIN) & MULTI-GEN (MAX)
+    pl.col("MULTI-GEN").alias("MULTI-GEN (MIN)"), 
+    pl.col("MULTI-GEN_right").alias("MULTI-GEN (MAX)")
+).sort("town", "decade")
+
 print(df_joined)
-df_joined.write_csv("output\sg_hdb_result.csv", include_header=True)
+
+df_joined.write_csv("output\sg_hdb_analysis_result.csv", include_header=True)
 
 print("Execution Time: %.2f seconds" % (time.time() - start_time))
